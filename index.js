5cc8011aa7fcdc2fd7689a637670e2a370c131c8
Dinesh Chukkala <dineshchukkala169@gmail.com>
2024-03-08 14:27:18 +0530

    Added basic get request as a demo

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    console.log(`[+] Received a GET request at /, with queries:`); 
+    console.table(req.query)
+
+    return res.send("<pre>Hello World!\nThis is how you create a route.</pre>");
+});
+
+app.listen(PORT, () => {
+    `[+] Server listening on PORT: ${PORT}`;
+});
