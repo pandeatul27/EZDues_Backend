5d4abb237f305cf0198568261eb71fd9f65732e7
jhasahab369 <jhabk369@gmail.com>
2024-04-03 02:23:35 +0530

    added most of admin & student routes

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require("express");
+const adminRoutes = require("./routes/adminRoutes");
+const studentRoutes = require("./routes/studentRoutes");
+const departmentRoutes= require("./routes/departmentRoutes");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
+
+
 const app = express();
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    console.log(`[+] Received a GET request at /, with queries:`);
-    console.table(req.query);
 
-    return res.send("<pre>Hello World!\nThis is how you create a route.</pre>");
-});
+
+app.use('/admin', adminRoutes);
+app.use('/student', studentRoutes);
+app.use('/department',departmentRoutes);
+
 
 app.listen(PORT, () => {
-    `[+] Server listening on PORT: ${PORT}`;
+    console.log(`[+] Server listening on PORT: ${PORT}`);
 });
