50d63d56f438b86c715c80209ce1c7c1093ef2d8
jhasahab369 <jhabk369@gmail.com>
2024-04-03 02:31:24 +0530

    Revert "added most of admin & student routes"
    This reverts commit 5d4abb237f305cf0198568261eb71fd9f65732e7.

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const express = require("express");
-const adminRoutes = require("./routes/adminRoutes");
-const studentRoutes = require("./routes/studentRoutes");
-const departmentRoutes= require("./routes/departmentRoutes");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-
-
 const app = express();
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    console.log(`[+] Received a GET request at /, with queries:`);
+    console.table(req.query);
 
-
-app.use('/admin', adminRoutes);
-app.use('/student', studentRoutes);
-app.use('/department',departmentRoutes);
-
+    return res.send("<pre>Hello World!\nThis is how you create a route.</pre>");
+});
 
 app.listen(PORT, () => {
-    console.log(`[+] Server listening on PORT: ${PORT}`);
+    `[+] Server listening on PORT: ${PORT}`;
 });
--- routes/departmentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const departmentController = require("../controllers/departmentController");
-
-
-
-module.exports = router;
